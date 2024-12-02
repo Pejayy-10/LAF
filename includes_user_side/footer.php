09bd4b8358337900f2d533a25e675d51bee83891
mard <bengilreymard@gmail.com>
2024-12-02 23:32:48 +0800

    hero changes, separation of lost_and_found css and user sidebar css, and making the div container eat whole page file

--- includes_user_side/footer.php
+++ includes_user_side/footer.php
@@ -1,10 +1,13 @@
     <!-- jQuery first (before any other scripts) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     
+    <!-- Typing -->
+    <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
     <!-- Then Bootstrap and its dependencies -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/your-fontawesome-kit.js"></script>
     <!-- Then DataTables -->
     <script type="text/javascript" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
@@ -13,6 +16,7 @@
     <script src="js/report.js"></script>
     <script src="js/main.js"></script>
     <script src="js/search.js"></script>
+    <script src="js/hero.js"></script>
     
     <script>
         $(document).ready(function() {
